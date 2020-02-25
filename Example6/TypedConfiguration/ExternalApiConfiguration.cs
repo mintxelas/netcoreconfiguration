318c744b915913282b509f5eee394911481e561c
mintxelas <fermin.saez@gmail.com>
2020-02-25 18:49:42 +0100

    example 6 - Strongly Typed Configuration

---
+++ Example6/TypedConfiguration/ExternalApiConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TypedConfiguration
+{
+    public class ExternalApiConfiguration
+    {
+        public string AuthorizationServer { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+
+    }
+}

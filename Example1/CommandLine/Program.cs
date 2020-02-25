db3c8c61b71d6a8154ef3d1dd5a3595b814764b0
mintxelas <fermin.saez@gmail.com>
2020-02-25 12:52:24 +0100

    example 1 - commandline

---
+++ Example1/CommandLine/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace CommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            Console.WriteLine(configuration["message"]);
+            Console.ReadLine();
+        }
+    }
+}

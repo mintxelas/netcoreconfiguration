31536f117fd3680ed18650f7fbd7283b18dd4250
mintxelas <fermin.saez@gmail.com>
2020-02-25 19:07:44 +0100

    example 7 - configuration injection and hot reload

---
+++ Example7/ConfigurationInjection/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ConfigurationInjection.Models;
+using Microsoft.Extensions.Options;
+
+namespace ConfigurationInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ExternalApiConfiguration configuration;
+        private readonly ExternalApiConfiguration configurationSnapshot;
+
+        public HomeController(IOptions<ExternalApiConfiguration> options, IOptionsSnapshot<ExternalApiConfiguration> optionsSnapshot)
+        {
+            configuration = options.Value;
+            configurationSnapshot = optionsSnapshot.Value;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = configuration.ClientId;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            ViewBag.Message = configurationSnapshot.ClientId;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Example7/ConfigurationInjection/ExternalApiConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfigurationInjection
+{
+    public class ExternalApiConfiguration
+    {
+        public string AuthorizationServer { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+
+    }
+}
